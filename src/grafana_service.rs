27878eb2d2bcc8d71ed490271c04b4ed8ec306a9
Alexey Velikiy <cg@corpglory.com>
2021-10-14 01:34:53 +0300

    extract metrics begin

--- src/grafana_service.rs
+++ src/grafana_service.rs
@@ -24,7 +24,21 @@ impl GrafanaService {
         let client = Client::new();
         let resp = client.request(req).await?;
         println!("Response: {}", resp.status());
+        Ok(())
+    }
 
+    pub async fn extract_metrics(&self) -> types::Result<()> {
+        let req = Request::builder()
+            .method(Method::POST)
+            .uri(self.url.clone() + "/api/datasources/proxy/1/api/v1/query_range")
+            .header("content-type", "application/json")
+            .header("Authorization", format!("Bearer {}", self.api_key))
+            .body(Body::from(r#"{"query":"go_memstats_alloc_bytes_total", "start": "1634163645", "end": "1634163945", "step": "15"}"#))
+            .unwrap();
+
+        let client = Client::new();
+        let resp = client.request(req).await?;
+        println!("Response: {}", resp.status());
         Ok(())
     }
 }
