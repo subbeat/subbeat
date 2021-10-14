92427a4a11bf0f4f2e542eb640851037c246b8ee
Alexey Velikiy <cg@corpglory.com>
2021-10-14 03:07:12 +0300

    some commit

--- src/grafana_service.rs
+++ src/grafana_service.rs
@@ -1,7 +1,11 @@
+use std::io::BufRead;
+
 use hyper::{Body, Client, Method, Request};
 use tokio::io::{stdout, AsyncWriteExt as _};
+use serde_json::json;
 
 use crate::types;
+use bytes::Buf as _;
 
 pub struct GrafanaService {
     url: String,
@@ -33,12 +37,36 @@ impl GrafanaService {
             .uri(self.url.clone() + "/api/datasources/proxy/1/api/v1/query_range")
             .header("content-type", "application/json")
             .header("Authorization", format!("Bearer {}", self.api_key))
-            .body(Body::from(r#"{"query":"go_memstats_alloc_bytes_total", "start": "1634163645", "end": "1634163945", "step": "15"}"#))
+            .body(Body::from(json!({ 
+                "query":"go_memstats_alloc_bytes_total", 
+                "start": "1634163645",
+                "end": "1634163945", 
+                "step": "15"
+            }).to_string())
+            )
+                
             .unwrap();
 
         let client = Client::new();
-        let resp = client.request(req).await?;
-        println!("Response: {}", resp.status());
+        let res = client.request(req).await?;
+        println!("Response: {}", res.status());
+            
+        // while let Some(next) = resp.body().data().await {
+        //     let chunk = next?;
+        //     io::stdout().write_all(&chunk).await?;
+        // }
+        
+        let body = hyper::body::aggregate(res).await?;
+        let mut reader = body.reader();
+        let mut line = String::new();
+        loop {
+            match reader.read_line(&mut line) {
+                Ok(_) => println!("{}", line),
+                Err(_) => break
+            }
+        }
+        
+
         Ok(())
     }
 }
